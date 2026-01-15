8560aebe2e04685a9bc4b4fca174b8d6636985dd
AhmadNawabi <ahmadshamoon0505@gmail.com>
2026-01-15 13:03:04 -0600

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,54 @@
+import Header from "./components/Header";
+import Section from "./components/Section";
+import HelpfulResource from "./components/HelpfulResource";
+import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
+
+
 function App() {
-  return <div className="app">Hello Atlas</div>;
+  return ( 
+    
+    
+    <div>
+      
+      <Header/>
+      <Section title="What is react?">
+        <p>
+          React is a JavaScript library used for building user interfaces.
+          It allows developers to create reusable UI components and efficiently
+          update and render components when data changes.
+        </p>  
+      </Section>
+
+      <Section title="Benefits of react">
+        <ul>
+          <li>Component-based architecture</li>
+          <li>Efficient rendering with the virtual DOM</li>
+          <li>Reusable and maintainable code</li>
+          <li>Strong community and ecosystem</li>
+          <li>Declarative and predictable UI updates</li>
+        </ul>
+      </Section>
+
+      <Section title="Helpful resources">
+        <HelpfulResource
+          label="React Official Documentation"
+          link="https://react.dev/learn"
+        />
+        <HelpfulResource
+          label="React Crash Course by Traversy Media"
+          link="https://www.youtube.com/watch?v=bMknfKXIFA8"
+        />
+        <HelpfulResource
+          label="freeCodeCamp React Course"
+          link="https://www.freecodecamp.org/learn/front-end-development-libraries/react/"
+        />
+      </Section>
+
+      <AboutMe />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

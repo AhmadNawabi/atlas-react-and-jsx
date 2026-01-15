72c4531d7400e43f7a2a6d7bfab3dba004e7826b
AhmadNawabi <ahmadshamoon0505@gmail.com>
2026-01-15 11:31:32 -0600

    Getting Started

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
 import "./index.css";
+import React from "react";
+import { createRoot } from "react-dom/client"
+import App from "./App"
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+        <App />
+        </React.StrictMode>
+);
 
 // Render the App component to the root element
